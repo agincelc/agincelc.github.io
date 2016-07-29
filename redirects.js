6d25bbed6fbc87089aa5799fd150c4a57cf23ca1
agincelc <adamzgc@gmail.com>
2016-07-29 16:25:19 -0400

    added itchio and james to redirects

--- redirects.js
+++ redirects.js
@@ -1,5 +1,7 @@
 function getRedirects() {
 	return [
 		{name: "sgdc", link: "http://sgdc.ml"},
+		{name: "itchio", link: "http://waveparadigm.itch.io/"},
+		{name: "james", link: "http://tehpilot.com"},
 	];
 }
