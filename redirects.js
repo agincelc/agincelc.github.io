f24da45919b3bc5f53d4ab17fdb2aa4170994eab
Adam Gincel <agincel@stevens.edu>
2016-08-08 14:16:09 -0400

    Added chat redirect.

--- redirects.js
+++ redirects.js
@@ -3,5 +3,6 @@ function getRedirects() {
 		{name: "sgdc", link: "http://sgdc.ml"},
 		{name: "itchio", link: "http://waveparadigm.itch.io/"},
 		{name: "james", link: "http://tehpilot.com"},
+		{name: "chat", link: "http://adamgincel.com/old/programming/adamtestchat.html"},
 	];
 }
