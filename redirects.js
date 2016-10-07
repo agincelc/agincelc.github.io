ec0f3e4507c59f8b47fc797e715b0fb8a9379408
Adam Gincel <agincel@stevens.edu>
2016-10-07 00:17:39 -0400

    added redirect for dad

--- redirects.js
+++ redirects.js
@@ -4,5 +4,6 @@ function getRedirects() {
 		{name: "itchio", link: "http://waveparadigm.itch.io/"},
 		{name: "james", link: "http://tehpilot.com"},
 		{name: "chat", link: "http://adamgincel.com/old/programming/adamtestchat.html"},
+		{name: "dad", link: "https://drive.google.com/file/d/0B4V6h-fMgMqTVWRJaWRQTFphcVU/view?usp=sharing"},
 	];
 }
