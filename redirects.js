d745d4ce33698d1dbf8824650da1d41399f154d5
Adam Gincel <agincel@stevens.edu>
2018-06-07 23:42:57 -0400

    moving to gamedevelopment.club and added index

--- redirects.js
+++ redirects.js
@@ -1,6 +1,6 @@
 function getRedirects() {
 	return [
-		{name: "sgdc", link: "http://sgdc.ml"},
+		{name: "sgdc", link: "http://sgdc.github.io"},
 		{name: "itchio", link: "http://waveparadigm.itch.io/"},
 		{name: "james", link: "http://tehpilot.com"},
 		{name: "chat", link: "http://adamgincel.com/old/programming/adamtestchat.html"},
